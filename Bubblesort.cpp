ca94d88d8e56b0f0a2b5dd5ab7cf3fc6a1cbb1de
Rakibul Islam <125082386+rakibulislamri276068@users.noreply.github.com>
2024-02-09 01:12:33 +0600

    Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,41 +1,33 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void sortarray(int arr[], int n){
-    int i;
-    for(i = 0;i<n;i++){
-        int temp;
-        int j;
-        for(j= 0;j<n-1;j++){
-            if(arr[j]>arr[j+1]){
-               temp = arr[j];
-               arr[j] = arr[j+1];
-               arr[j+1] = temp;
-
-            }
-        }
-    }
-
-}
-
-
 void printarray(int arr[], int n){
-    for(int i = 0 ;i< n ; i++ ){
-        cout<<arr[i]<<"\t";
+    for(int i = 0; i<n ; i++){
+        cout<<arr[i]<<"  ";
     }
+}
 
+void sortarray(int arr[], int n){
+for(int i = 0 ;i< n ;i++)
+ 
+for(int j = i + 1 ;j<n ;j ++){
+    if ( arr[i]>arr[j]){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp ;
+    }
+}
 }
 
 
-int main()
-{
-    cout<<"Bubble Sort.";
-    int n=6;
-    int arr[] = {5,3,8,4,9,6};
-    cout<<"Unsorted array is :";
-    printarray(arr,n);
+int main(){
+    cout<<"Bubble sort\n";
+    int arr[] = {6,9,0,2,4,1,3};
+    int n = 7;
+    cout<<"Unsorted Arra is:";
+    printarray(arr, n);
 
-    sortarray(arr, n);
-    cout<<"Sorted Array is ";
+    sortarray(arr,n);
+    cout<<"\nSorted Array is :";
     printarray(arr,n);
 }
